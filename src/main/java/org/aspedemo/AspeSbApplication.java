831a47de5ab380858e1219083f49646d336cf502
Jan Trukenmüller <truj@users.noreply.github.com>
2021-02-07 11:18:15 +0100

    initial commit

---
+++ src/main/java/org/aspedemo/AspeSbApplication.java
@@ -0,0 +1,13 @@
+package org.aspedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspeSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspeSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aspedemo/AspeSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aspedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspeSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
